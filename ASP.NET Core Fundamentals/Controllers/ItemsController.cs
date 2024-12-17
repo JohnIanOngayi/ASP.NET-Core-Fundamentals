df050f7a0b7ab4b7ff1a3591be81098b148918a2
ONG'AYI JOHNIAN <ongayijohnian@gmail.com>
2024-12-17 12:47:35 +0300

    Added items MVC files - Learnt how to embed in asp

---
+++ ASP.NET Core Fundamentals/Controllers/ItemsController.cs
@@ -0,0 +1,19 @@
+﻿using ASP.NET_Core_Fundamentals.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_Fundamentals.Controllers
+{
+	public class ItemsController : Controller
+	{
+		public IActionResult Overview()
+		{
+			var item = new Item() { Name = "Keyboard" };
+			return View(item);
+		}
+
+		public IActionResult Edit(int id)
+		{
+			return Content($"id: {id}");
+		}
+	}
+}
---
+++ ASP.NET Core Fundamentals/Models/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP.NET_Core_Fundamentals.Models
+{
+	public class Item
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+	}
+}
