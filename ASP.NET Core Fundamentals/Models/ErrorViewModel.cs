8ebd701c9031dc89d56254f727f29ccefc556cdd
ONG'AYI JOHNIAN <ongayijohnian@gmail.com>
2024-12-17 08:02:19 +0300

    Add project files.

---
+++ ASP.NET Core Fundamentals/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_Core_Fundamentals.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
